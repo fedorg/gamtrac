dd078916d887c23a4efd472a498176ce5ca5ee90
Golishevskii Fedor <golishevskii@biocad.ru>
2019-04-26 17:48:49 +0300

    serves some results

--- api.go
+++ api.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
+// TODO: remove this global state
+
 type ServerState struct {
-	files map[string]annotResult
+	sync.Mutex
+	files map[string]*AnnotResult
 }
+
+var srvState *ServerState
 
 // Serve ... run the server and persistence layer
 func Serve(state *ServerState) {
-	http.HandleFunc("/api/index", indexHandler)
-	http.HandleFunc("/api/repo/", repoHandler)
+	srvState = state
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/getall/", repoHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	//...
-	w.WriteHeader(400)
+	w.WriteHeader(200)
 	w.Write([]byte("hi, this is patrick"))
 }
 
 func repoHandler(w http.ResponseWriter, r *http.Request) {
 	//...
+	srvState.Lock()
+	ret, err := json.MarshalIndent(srvState, "", " ")
+	srvState.Unlock()
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf("400 Unhandled exception: %v", err)))
+		return
+	}
 	w.WriteHeader(200)
-	w.Write([]byte("Bye"))
+	w.Write([]byte(ret))
 }
--- gamtrac.go
+++ gamtrac.go
@@ -3,11 +3,10 @@ package main
 import (
 	"bufio"
 	"crypto/sha256"
-	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"os"
-	"os/user"
 	"path/filepath"
 	"runtime"
 	"sync"
@@ -17,30 +16,76 @@ import (
 type annotItem struct {
 	path     string
 	fileInfo os.FileInfo
-	owner    *user.User
 	rules    []ruleMatcher
+	queuedAt time.Time
 }
 
-type annotResult struct {
-	path    string
-	IsDir   bool
-	ModTime time.Time
-	Size    uint64
-	Mode    uint32
-	hash    hashDigest
-	pattern string
-	parsed  map[string]string
+type HashDigest struct {
+	Algorithm string
+	Value     []byte
 }
 
-type hashDigest []byte
+func (h HashDigest) String() string {
+	return fmt.Sprintf("%s:%s", h.Algorithm, hex.EncodeToString(h.Value))
+	// return fmt.Sprintf(base64.StdEncoding.EncodeToString(h.Value))
+}
+
+type FileError struct {
+	// Filename  string
+	Error     error
+	CreatedAt time.Time
+}
+
+type AnnotResult struct {
+	Path        string             // required
+	Size        int64              // required
+	Mode        os.FileMode        // required
+	ModTime     time.Time          // required
+	QueuedAt    time.Time          // required
+	ProcessedAt time.Time          // required
+	IsDir       bool               // required
+	OwnerUID    *string            // optional
+	Hash        *HashDigest        // optional
+	Pattern     *string            // optional
+	Parsed      *map[string]string // optional
+	Errors      []FileError        // required
+}
+
+func NewAnnotResult(
+	Path string,
+	Size int64,
+	Mode os.FileMode,
+	ModTime time.Time,
+	QueuedAt time.Time,
+	ProcessedAt time.Time,
+	IsDir bool,
+	OwnerUID *string,
+	Hash *HashDigest,
+	Pattern *string,
+	Parsed *map[string]string,
+	Errors []FileError,
+) AnnotResult {
+	return AnnotResult{
+		Path:        Path,
+		Size:        Size,
+		Mode:        Mode,
+		ModTime:     ModTime,
+		QueuedAt:    QueuedAt,
+		ProcessedAt: ProcessedAt,
+		IsDir:       IsDir,
+		OwnerUID:    OwnerUID,
+		Hash:        Hash,
+		Pattern:     Pattern,
+		Parsed:      Parsed,
+		Errors:      Errors,
+	}
+}
 
 var test_rules = []string{
 	"<дата>_<проект>_<методика>_<вид данных>_<наименование образца>_<комментарий>",
 	"R:\\DAR\\LAM\\Screening group\\<Заказчик>\\1_Результаты, протоколы, отчеты\\<Измеряемый параметр>_<Метод анализа>\\<Проект>\\"}
 
-// TODO: check if the worker pool is truly parallel by testing it with time delays
-
-func computeHash(path string) (hashDigest, error) {
+func computeHash(path string) (*HashDigest, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -52,40 +97,88 @@ func computeHash(path string) (hashDigest, error) {
 	if err != nil {
 		return nil, err
 	}
-	digest := h.Sum(nil)
+	digest := &HashDigest{Value: h.Sum(nil), Algorithm: "sha256"}
 	return digest, nil
 }
 
-func processFile(inputs <-chan annotItem, output chan<- annotResult, errorsChan chan<- error, wg *sync.WaitGroup) {
+func NewFileError(err error) FileError {
+	return FileError{
+		// Filename:  filename,
+		Error:     err,
+		CreatedAt: time.Now(),
+	}
+}
+
+func processFile(inputs <-chan annotItem, output chan<- *AnnotResult, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for input := range inputs {
 		filename := input.path
 		info := input.fileInfo
-		// fmt.Println("Recv files: ", filename)
-		if info.IsDir() {
-			continue
-		}
+		fmt.Println("Processing file: ", filename)
+		var rule *string
+		var ruleVars map[string]string
+		// TODO: run these as goroutines in parallel
+		errors := []FileError{}
 		parsed, err := ParseFilename(filename, input.rules, true)
 		if err != nil {
-			errorsChan <- err
-			continue
+			errors = append(errors, NewFileError(err))
+		} else {
+			rule = &parsed.rule.rule
+			ruleVars = parsed.AsMap()
+		}
+		owner, err := GetFileOwnerUID(filename)
+		if err != nil {
+			errors = append(errors, NewFileError(err))
 		}
 		hash, err := computeHash(filename)
 		if err != nil {
-			// errorsChan <- err
-			// continue
-			hash = nil
+			errors = append(errors, NewFileError(err))
 		}
-		ret := annotResult{path: filename, pattern: parsed.rule.rule, parsed: parsed.AsMap(), hash: hash}
-		// fmt.Println("Send for files: ", filename)
-		output <- ret
+		ret := NewAnnotResult(filename, info.Size(), info.Mode(), info.ModTime(), input.queuedAt, time.Now(), info.IsDir(), owner, hash, rule, &ruleVars, errors)
+		fmt.Println("Finished processing file: ", filename)
+		output <- &ret
 	}
 }
 
+func collectResults(annots <-chan *AnnotResult, out chan<- map[string]*AnnotResult) {
+	// TODO: make this less ugly
+	ret := map[string]*AnnotResult{}
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	m := &sync.Mutex{}
+	set := func(filename string, rslt *AnnotResult) {
+		m.Lock()
+		defer m.Unlock()
+		old, exists := ret[filename]
+		if !exists {
+			ret[filename] = rslt
+		} else {
+			if old.ProcessedAt.Before(rslt.ProcessedAt) {
+				ret[filename] = rslt
+			}
+		}
+	}
+	go func() {
+		defer wg.Done()
+		for an := range annots {
+			fmt.Printf("Collecting %v", an.Path)
+			set(an.Path, an)
+		}
+	}()
+	wg.Wait()
+	out <- ret
+}
+
 func main() {
+
+	srvState := ServerState{
+		files: make(map[string]*AnnotResult),
+	}
+	go Serve(&srvState)
+
 	username := os.Getenv("GAMTRAC_USERNAME")
 	pass := os.Getenv("GAMTRAC_PASSWORD")
-	// rslt := map[string]annotResult{}
+	// rslt := map[string]AnnotResult{}
 	owner, err := GetFileOwnerUID(`C:\Users\fed00\Desktop\2019.02.19 DI FAVEA\03-Data-Management-and-Integrity-3-RU.pdf`)
 	owner, err = GetFileOwnerUID("testdata.csv")
 	owner, err = GetFileOwnerUID(`R:\DAR\ОБИ\archive\Raw Data Guava S1.3.L32-24.004 (А-0005492)\Raw Data\2018-10-20_test.fcs`)
@@ -110,67 +203,61 @@ func main() {
 		panic(err)
 	}
 	fmt.Print(*owner)
-	// Serve()
-	csv, err := ReadCSVTable("testdata.csv")
-	if err != nil {
-		panic(err)
-	}
-	rules, err := CSVToRules(csv, true)
-	if err != nil {
-		panic(err)
-	}
 
-	args := os.Args[1:]
-	var paths []string
-	if len(args) == 0 {
-		paths = append(paths, ".")
-	} else {
-		paths = append(paths, args...)
-	}
-	inputs := make(chan annotItem)
-	output := make(chan annotResult)
-	errorsChan := make(chan error)
+	var epoch int64 = 0
 
-	wg := &sync.WaitGroup{}
-	numWorkers := runtime.NumCPU()
-	wg.Add(numWorkers)
-	for w := 0; w < numWorkers; w++ {
-		go processFile(inputs, output, errorsChan, wg)
-	}
-
-	printResults := func(annots <-chan annotResult, errs <-chan error, wg *sync.WaitGroup) {
-		defer wg.Done()
-		for {
-			select {
-			case an := <-annots:
-				// fmt.Println(hex.EncodeToString(an.hash))
-				fmt.Println(base64.StdEncoding.EncodeToString(an.hash), an.path, an.pattern)
-				fmt.Println(an.parsed)
-				// fmt.Println("Printing results: ", an)
-			case err := <-errs:
-				fmt.Fprintln(os.Stderr, err)
-			}
+	for {
+		epoch++
+		fmt.Printf("Epoch %v\n", epoch)
+		csv, err := ReadCSVTable("testdata.csv")
+		if err != nil {
+			panic(err)
 		}
+		rules, err := CSVToRules(csv, true)
+		if err != nil {
+			panic(err)
+		}
+
+		args := os.Args[1:]
+		var paths []string
+		if len(args) == 0 {
+			paths = append(paths, ".")
+		} else {
+			paths = append(paths, args...)
+		}
+		inputs := make(chan annotItem)
+		output := make(chan *AnnotResult)
+		// errorsChan := make(chan FileError)
+
+		wg := &sync.WaitGroup{}
+		numWorkers := runtime.NumCPU()
+		wg.Add(numWorkers)
+		for w := 0; w < numWorkers; w++ {
+			go processFile(inputs, output, wg)
+		}
+
+		done := make(chan map[string]*AnnotResult)
+		go collectResults(output, done)
+
+		for _, p := range paths {
+			filepath.Walk(p, func(path string, f os.FileInfo, err error) error {
+				if err != nil {
+					fmt.Printf("Error: %s\n", err.Error())
+					return err
+				}
+				// fmt.Printf("Queued: %s\n", path)
+				func() { inputs <- annotItem{path: path, fileInfo: f, queuedAt: time.Now(), rules: rules} }()
+				return nil
+			})
+		}
+
+		close(inputs)
+		wg.Wait()
+		rslt := <-done
+		srvState.Lock()
+		srvState.files = rslt
+		srvState.Unlock()
 	}
-
-	// wg.Add(1)
-	go printResults(output, errorsChan, wg)
-	// <-done
-
-	for _, p := range paths {
-		filepath.Walk(p, func(path string, f os.FileInfo, err error) error {
-			if err != nil {
-				fmt.Printf("Error: %s\n", err.Error())
-				return err
-			}
-			// fmt.Printf("Queued: %s\n", path)
-			func() { inputs <- annotItem{path: path, fileInfo: f, rules: rules} }()
-			return nil
-		})
-	}
-
-	close(inputs)
-	wg.Wait()
 	// TODO: wait for all the result combos to finish
 
 	// wait for everyone
