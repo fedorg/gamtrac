bff49a088192d9fef51c23fa5bf3af1d0488dd55
Fedor Golishevskiy <fed0000r@gmail.com>
2019-05-06 12:27:55 +0300

    pushes actual files to the server

--- gamtrac.go
+++ gamtrac.go
@@ -3,12 +3,12 @@ package main
 import (
 	"bufio"
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"gamtrac/api"
 	"gamtrac/rules"
 	"gamtrac/scanner"
 	"io"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -118,49 +118,30 @@ func collectResults(annots <-chan *AnnotResult, out chan<- map[string]*AnnotResu
 	out <- ret
 }
 
+func PushFileUpdates(gg *api.GamtracGql, revision int, rslts map[string]*AnnotResult) ([]api.Files, error) {
+	newFiles := make([]Files, len(rslts))
+	i := 0
+	for filename, r := range rslts {
+		data, err := json.Marshal(r)
+		if err != nil {
+			return nil, err
+		}
+		jsonStr := string(data)
+		newFiles[i] = Files{
+			Filename: filename,
+			Revision: revision,
+			Data:     jsonStr,
+		}
+		i++
+	}
+	insertedFiles, err := gg.RunInsertFiles(newFiles)
+	if err != nil {
+		return nil, err
+	}
+	return insertedFiles, nil
+}
+
 func main() {
-	gg := api.NewGamtracGql("https://fedor-hasura-test.herokuapp.com/v1alpha1/graphql", 5000, false)
-	rand.Seed(time.Now().UnixNano())
-	alphabet := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-	randSeq := func(n int) string {
-		b := make([]rune, n)
-		for i := range b {
-			b[i] = alphabet[rand.Intn(len(alphabet))]
-		}
-		return string(b)
-	}
-
-	numNewFiles := rand.Intn(100)
-	_nf := make([]Files, numNewFiles)
-	for i := 0; i < numNewFiles; i++ {
-		_nf[i] = Files{
-			Filename: randSeq(25),
-			Revision: rand.Intn(66),
-			Data:     "{}",
-		}
-	}
-	newFiles, err := gg.RunInsertFiles(_nf)
-	if err != nil {
-		panic(err)
-	}
-	for _, nf := range newFiles {
-		fmt.Println(nf.FileID, nf.Revision)
-	}
-	fmt.Println("Deleting files...")
-	if _, err := gg.RunDeleteFiles(10); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Fetching files ...")
-	files, err := gg.RunFetchFiles(10)
-	if err != nil {
-		panic(err)
-	}
-	for i := range files {
-		fmt.Println(files[i].FileID, files[i].Filename)
-	}
-	return
-
 	srvState := api.NewServerState()
 	go api.Serve(srvState)
 
@@ -192,7 +173,8 @@ func main() {
 	// }
 	// fmt.Print(*owner)
 
-	var epoch int64 = 0
+	var epoch int64 = 0 // TODO create revision records on the server
+	gg := api.NewGamtracGql("https://fedor-hasura-test.herokuapp.com/v1alpha1/graphql", 5000, false)
 
 	for {
 		epoch++
@@ -244,6 +226,13 @@ func main() {
 		wg.Wait()
 		close(output)
 		rslt := <-done
+		newFiles, err := PushFileUpdates(gg, int(epoch), rslt)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot update files on server:\n%v\n", err)
+		}
+		for _, nf := range newFiles {
+			fmt.Printf("%6d| %v\n", nf.FileID, nf.Filename)
+		}
 		srvState.Update(rslt)
 		time.Sleep(time.Second * 1)
 	}
